c997bc860c14f7549858745c5fdce9cee7b5b03c
tushal012 <tushalkashyap11@gmail.com>
2021-05-15 20:44:26 +0530

    display power of two

---
+++ powerOfTwo.sh
@@ -0,0 +1,17 @@
+
+#! /bin/bash -sh
+
+
+read -p "enter the number"
+$i = 0;                
+powerOfTwo = 1;       
+
+        
+        while [ $i -lt n ]
+do 
+            echo $i   
+            powerOfTwo = 2 * powerOfTwo;                
+            i = $(( i +1 ))
+done
+
+
